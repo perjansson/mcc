734962894e9ec0eda0e94c79d6143ea54d7e6981
Per Jansson <per.r.jansson@gmail.com>
2013-10-29 19:13:36 +0100

    Moved app.js and constants.js

---
+++ app/constants.js
@@ -0,0 +1,13 @@
+app.constant('constants', {
+    versionNumber: '1.3.6',
+    numberOfAttendeesText: null,
+    averageHourlyRateText: null,
+    currencyText: null,
+    meetingCostTextUpdateIntervalInMillis: '50',
+    backendUpdateIntervalInMillis: '10000',
+    shouldPersistMeetings: true,
+    shouldUseNodeJs: true,
+    shouldUseSpringMvc: false,
+    nodeJsBackendHost: "ws://mcc.cloudno.de/",
+    /*nodeJsBackendHost: "127.0.0.1:1337/",*/
+});
