0e8662e185e21b0b6ec85e03603a158c5e082670
Per Jansson <per.r.jansson@gmail.com>
2013-11-10 22:40:11 +0100

    Aligning numeric columns right

--- app/scripts/controllers/toplist.js
+++ app/scripts/controllers/toplist.js
@@ -1,29 +1,4 @@
 app.controller('TopListCtrl', function ($scope, $location, constants, socketioMeetingService) {
-
-	/*$scope.toplist =
-	[
-	    {
-	        "name": "Meeting 1",
-	        "numberOfAttendees": "10",
-	        "averageHourlyRate": "1000",
-	        "currency": "SEK",
-	        "meetingCost": "10000"
-	    },
-	    {
-	        "name": "Meeting 2",
-	        "numberOfAttendees": "20",
-	        "averageHourlyRate": "1000",
-	        "currency": "SEK",
-	        "meetingCost": "20000"
-	    },
-	    {
-	        "name": "Meeting 3",
-	        "numberOfAttendees": "30",
-	        "averageHourlyRate": "1000",
-	        "currency": "SEK",
-	        "meetingCost": "30000"
-	    }
-	]*/
     
     if (constants.shouldPersistMeetings) {
         if (constants.shouldUseNodeJs) {
@@ -40,7 +15,7 @@ app.controller('TopListCtrl', function ($scope, $location, constants, socketioMe
                 $('#connection-indicator').addClass('disconnected').removeClass('connected').removeClass('connecting');
             };
             var onTopListUpdatedCallback = function(toplist) {
-            	toplist.sort(function(a,b) { return parseFloat(b.meetingCost) - parseFloat(a.meetingCost) } );
+            	toplist.sort(function(a,b) { return parseFloat(b.comparableMeetingCost) - parseFloat(a.comparableMeetingCost) } );
                 console.log('On toplist update');
                 $scope.toplist = toplist;
         		$scope.$apply();
