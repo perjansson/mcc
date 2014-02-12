3eb0adccfac30f3f106461981610275a16acabf7
Per Jansson <per.r.jansson@gmail.com>
2014-02-12 22:25:35 +0100

    Bumped up version to 1.7.0

--- app/scripts/constants.js
+++ app/scripts/constants.js
@@ -1,11 +1,11 @@
 app.constant('constants', {
-    versionNumber: '1.6.3',
+    versionNumber: '1.7.0',
     numberOfAttendeesText: null,
     averageHourlyRateText: null,
     currencyText: 'BTC',
     meetingCostTextUpdateIntervalInMillis: '50',
     backendUpdateIntervalInMillis: '60000',
-    //nodeJsBackendHost: "ws://mcc.cloudno.de/",
-    nodeJsBackendHost: "127.0.0.1:1337/",
+    nodeJsBackendHost: "ws://mcc.cloudno.de/",
+    //nodeJsBackendHost: "127.0.0.1:1337/",
     sharingUrl: '/meeting/'
 });
