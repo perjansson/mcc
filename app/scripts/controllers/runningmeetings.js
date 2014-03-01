464afc8e8563f6e8af0b659635d5a2148122110a
Per Jansson <per.r.jansson@gmail.com>
2014-03-01 22:48:39 +0100

    Showing totals on running meetings

--- app/scripts/controllers/runningmeetings.js
+++ app/scripts/controllers/runningmeetings.js
@@ -10,15 +10,45 @@ app.controller('RunningMeetingsCtrl', function ($rootScope, $scope, $location, c
             return parseFloat(b.comparableMeetingCost) - parseFloat(a.comparableMeetingCost)
         });
         $scope.runningMeetings = runningMeetings;
-        $scope.totalComparableMeetingCost = calculateTotalCost(runningMeetings);
+        calculateTotals(runningMeetings);
     }
 
-    function calculateTotalCost(runningMeetings) {
+    function calculateTotals(runningMeetings) {
         var totalCost = null;
+        var totalNumberOfAttendees = null;
+        var totalTimeInHours = null;
         runningMeetings.forEach(function (meeting) {
             totalCost = totalCost + parseFloat(meeting.comparableMeetingCost);
+            totalNumberOfAttendees = totalNumberOfAttendees + parseInt(meeting.numberOfAttendees);
+            totalTimeInHours = totalTimeInHours + (meeting.meetingCost / meeting.numberOfAttendees / meeting.averageHourlyRate);
         });
-        return totalCost + ' ' + constants.currencyText;
+        $scope.totalComparableMeetingCost = totalCost + ' ' + constants.currencyText;
+        $scope.totalNumberOfAttendees = totalNumberOfAttendees;
+        $scope.totalPrettyMeetingDuration = timeInHoursToPrettyMeetingDuration(totalTimeInHours);
+    }
+
+    const decimalToTimeFactor = 0.6;
+
+    function timeInHoursToPrettyMeetingDuration(timeInHours) {
+        var prettyMeetingDuration = null;
+        var hours, minutes, seconds = null;
+        if (timeInHours >= 1) {
+            var array = roundToDecimals(timeInHours, 2).toString().split('.');
+            hours = parseInt(array[0]);
+            minutes = roundToDecimals(parseInt(array[1]) * decimalToTimeFactor, 0);
+            prettyMeetingDuration = hours + " h " + minutes + " min";
+        } else if (timeInHours >= 0.01666666666667) {
+            minutes = timeInHours * 60;
+            prettyMeetingDuration = roundToDecimals(minutes, 0) + " min";
+        } else {
+            seconds = timeInHours * 3600;
+            prettyMeetingDuration = roundToDecimals(seconds, 0) + " s";
+        }
+        return prettyMeetingDuration;
+    }
+
+    function roundToDecimals(value, numberOfDecimals) {
+        return (Math.round(value * 100000) / 100000).toFixed(numberOfDecimals);
     }
 
     var runningMeetings = meetingService.tryToGetRunningMeetings();
